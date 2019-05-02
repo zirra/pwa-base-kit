439879307178d0fe8e07dffca93d0dbe02041ea7
zirra <msullivan@heysulli.com>
2019-05-02 09:07:10 -0500

    Setting up PWA for Vue base kit

---
+++ src/store/modules/appbase.js
@@ -0,0 +1,20 @@
+import { storage } from '../../utils/storage'
+
+const state = {
+}
+
+const mutations = {
+}
+
+const getters = {
+}
+
+const actions = {
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
