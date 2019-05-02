88d9a1fa63531a41b66f452c1ed7e4a0969894ab
zirra <msullivan@heysulli.com>
2019-05-02 09:10:10 -0500

    updating main to support store

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+
 import App from './App'
 import router from './router'
+import store from './store'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+const vm = new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
+
+export {vm}
